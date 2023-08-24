568c10d03f01db248729448feab11a305cff7b68
titisda <titisda@users.noreply.github.com>
2023-08-24 13:16:45 +0000

    deploy: c4c37a6806256abfc7ec44c7e9180af49229e5c2

---
+++ 8461.f2150fdc31db14c3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[8461],{8461:(_,t,e)=>{e.r(t),e.d(t,{SafeAreaWeb:()=>a});var n=e(5861),r=e(7423);class a extends r.Uw{getStatusBarHeight(){return(0,n.Z)(function*(){return{height:0}})()}getSafeAreaInsets(){return(0,n.Z)(function*(){return{top:0,bottom:0,left:0,right:0}})()}}}}]);
